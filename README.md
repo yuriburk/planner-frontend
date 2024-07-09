5e68b49d3e4111f8d8032fa2f012f85e06ed26ee
Jefferson Yuri Cunha Burk <jefferson.burk@ambevtech.com.br>
2024-07-09 11:45:16 -0300

    Adicionando modal para convidar emails

--- README.md
+++ README.md
@@ -2,4 +2,3 @@ O projeto é um site desktop para montar planos de viagem com amigos, registrar 
 Esse é um dos projetos do evento NLW Journey, um dos conteúdos disponíveis para alunos da Rocketseat.
 
 ![image](https://github.com/yuriburk/planner-frontend/assets/11825151/693bf555-14c5-4c91-bcc1-a6b28af79251)
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import {
   ArrowRight,
+  AtSign,
   Calendar,
+  ChevronDown,
   MapPin,
   Settings2,
   UserRound,
+  X,
 } from 'lucide-react';
 
 const App = () => {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
 
   const openGuestsInput = () => {
     setIsGuestsInputOpen(true);
@@ -16,6 +21,33 @@ const App = () => {
 
   const closeGuestsInput = () => {
     setIsGuestsInputOpen(false);
+  };
+
+  const openGuestsModal = () => {
+    setIsGuestsModalOpen(true);
+  };
+
+  const closeGuestsModal = () => {
+    setIsGuestsModalOpen(false);
+  };
+
+  const addNewEmailToInvite = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email')?.toString();
+
+    if (!email || emailsToInvite.includes(email)) {
+      return;
+    }
+
+    setEmailsToInvite((emails) => [...emails, email]);
+
+    event.currentTarget.reset();
+  };
+
+  const removeEmailFromInvite = (email: string) => {
+    setEmailsToInvite((emails) => emails.filter((e) => e !== email));
   };
 
   return (
@@ -73,14 +105,15 @@ const App = () => {
 
           {isGuestsInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <div className="flex items-center gap-2 flex-1">
+              <button
+                onClick={openGuestsModal}
+                className="flex items-center gap-2 flex-1 text-left"
+              >
                 <UserRound className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  placeholder="Quem estará na viagem?"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
+                <p className="text-lg text-zinc-400 flex-1">
+                  Quem estará na viagem?
+                </p>
+              </button>
 
               <div className="w-px h-6 bg-zinc-800" />
 
@@ -106,6 +139,67 @@ const App = () => {
           .
         </p>
       </div>
+
+      {isGuestsModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecionar convidados</h2>
+                <button onClick={closeGuestsModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <span className="text-sm text-zinc-400">
+                Os convidados irão receber e-mails para confirmar a participação
+                na viagem.
+              </span>
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {emailsToInvite.map((email) => (
+                <div
+                  key={email}
+                  className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
+                >
+                  <span className="text-zinc-300">{email}</span>
+                  <button
+                    onClick={() => {
+                      removeEmailFromInvite(email);
+                    }}
+                  >
+                    <X className="size-4 text-zinc-400" />
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <div className="w-full h-px bg-zinc-800 " />
+
+            <form
+              onSubmit={addNewEmailToInvite}
+              className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2"
+            >
+              <div className="px-2 flex flex-1 items-center gap-2">
+                <AtSign className="text-zinc-400 size-5" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Digite o e-mail do convidado"
+                  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
+                Convidar
+                <ChevronDown className="size-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
